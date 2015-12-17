a2643044c57cd933c9896fef7d284a89eda16177
switchkiller <emailbarung@gmail.com>
2015-12-18 02:12:14 +0545

    Update dashboard at Fri Dec 18 02:12:14 NPT 2015

---
+++ script.js
@@ -0,0 +1,12 @@
+var ctx = document.getElementById("chart").getContext("2d");
+var myLineChart = new Chart(ctx).Line(data, {
+    tooltipTemplate = function(valuesObject){
+      var label = valuesObject.label;
+      var idx = data.labels.indexOf(label);
+      var result = data.datasets[0].time[idx];
+      if (data.datasets[0].languages[idx] !== 0)
+        result += '[' + data.datasets[0].languages[idx].join(", ") + "]"
+      return result;
+    }
+});
+document.getElementById("summary").innerHTML = "I  have written code for " + totalTime + " in the the last week in mostly " + languages.join(", ") + ".";
