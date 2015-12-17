f6d1ad57edf9f97fc9d304cff0d51a56149e9843
switchkiller <emailbarung@gmail.com>
2015-12-18 02:06:58 +0545

    Automation with script

---
+++ halotracker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+python time.py
+cat chart_script.js >> _script.js
+git stash
+git checkout gh-pages
+mv _script.js script.js
+git add script.js
+git commit -m "Update dashboard at `date`"
+git push -f origin gh-pages
+git checkout master
+git stash pop
