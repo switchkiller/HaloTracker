84168de0f9abf2b436aa0c304b68d054dec322e7
switchkiller <emailbarung@gmail.com>
2016-02-06 22:17:48 +0530

    Update dashboard at Sat Feb  6 22:17:48 IST 2016

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-var data = {"labels": ["16 December", "17 December", "18 December", "19 December", "20 December", "21 December", "22 December"], "datasets": [{"languages": [[], ["Python", "Other"], ["Python", "JavaScript", "HTML"], ["Java", "XML", "Other"], ["Python", "JavaScript", "C++"], ["Python"], ["JavaScript"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": [" 0 secs", "53 mins", "3 hrs 3 mins", "1 hr 13 mins", "2 hrs 24 mins", "3 mins", "37 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [0.0, 0.8852777777777778, 3.069722222222222, 1.2344444444444445, 2.4077777777777776, 0.06277777777777778, 0.6186111111111111], "label": "Dataset"}]};
-var totalTime = "8 hours 16 minutes";
-var languages = ["Python", "JavaScript", "Java"];
 var ctx = document.getElementById("chart").getContext("2d");
 var myLineChart = new Chart(ctx).Line(data, {
   tooltipTemplate: function(valuesObject){
